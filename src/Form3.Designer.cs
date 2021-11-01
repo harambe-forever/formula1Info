c38a1c3a5dd908cfd3147a24fb78fbddf3ed2eaf
Mega <71318378+harambe-forever@users.noreply.github.com>
2021-11-01 17:34:08 +0300

    Add files via upload

---
+++ src/Form3.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace FormulaAddict
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitForm3 = new System.Windows.Forms.Button();
+            this.alfaRomeo = new System.Windows.Forms.Button();
+            this.tauri = new System.Windows.Forms.Button();
+            this.alpine = new System.Windows.Forms.Button();
+            this.aston = new System.Windows.Forms.Button();
+            this.sf = new System.Windows.Forms.Button();
+            this.haas = new System.Windows.Forms.Button();
+            this.mclaren = new System.Windows.Forms.Button();
+            this.mercedes = new System.Windows.Forms.Button();
+            this.rb = new System.Windows.Forms.Button();
+            this.williams = new System.Windows.Forms.Button();
+            this.drivers = new System.Windows.Forms.Button();
+            this.keys = new System.Windows.Forms.TextBox();
+            this.values = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // exitForm3
+            // 
+            this.exitForm3.BackColor = System.Drawing.Color.Transparent;
+            this.exitForm3.Font = new System.Drawing.Font("Stencil", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.exitForm3.Location = new System.Drawing.Point(1134, 0);
+            this.exitForm3.Name = "exitForm3";
+            this.exitForm3.Size = new System.Drawing.Size(66, 66);
+            this.exitForm3.TabIndex = 1;
+            this.exitForm3.Text = "X";
+            this.exitForm3.UseVisualStyleBackColor = false;
+            this.exitForm3.Click += new System.EventHandler(this.exitForm3_Click);
+            // 
+            // alfaRomeo
+            // 
+            this.alfaRomeo.BackgroundImage = global::FormulaAddict.Properties.Resources.alfaRomeo;
+            this.alfaRomeo.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.alfaRomeo.Location = new System.Drawing.Point(0, 0);
+            this.alfaRomeo.Name = "alfaRomeo";
+            this.alfaRomeo.Size = new System.Drawing.Size(412, 122);
+            this.alfaRomeo.TabIndex = 2;
+            this.alfaRomeo.Text = "Alfa Romeo";
+            this.alfaRomeo.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.alfaRomeo.UseVisualStyleBackColor = true;
+            this.alfaRomeo.Click += new System.EventHandler(this.alfaRomeo_Click);
+            // 
+            // tauri
+            // 
+            this.tauri.BackgroundImage = global::FormulaAddict.Properties.Resources.tauri;
+            this.tauri.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tauri.Location = new System.Drawing.Point(0, 122);
+            this.tauri.Name = "tauri";
+            this.tauri.Size = new System.Drawing.Size(412, 122);
+            this.tauri.TabIndex = 3;
+            this.tauri.Text = "AlphaTauri";
+            this.tauri.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.tauri.UseVisualStyleBackColor = true;
+            this.tauri.Click += new System.EventHandler(this.tauri_Click);
+            // 
+            // alpine
+            // 
+            this.alpine.BackgroundImage = global::FormulaAddict.Properties.Resources.alpine;
+            this.alpine.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.alpine.Location = new System.Drawing.Point(0, 244);
+            this.alpine.Name = "alpine";
+            this.alpine.Size = new System.Drawing.Size(412, 122);
+            this.alpine.TabIndex = 4;
+            this.alpine.Text = "Alpine";
+            this.alpine.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.alpine.UseVisualStyleBackColor = true;
+            this.alpine.Click += new System.EventHandler(this.alpine_Click);
+            // 
+            // aston
+            // 
+            this.aston.BackgroundImage = global::FormulaAddict.Properties.Resources.aston;
+            this.aston.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.aston.Location = new System.Drawing.Point(0, 366);
+            this.aston.Name = "aston";
+            this.aston.Size = new System.Drawing.Size(412, 122);
+            this.aston.TabIndex = 5;
+            this.aston.Text = "Aston Martin";
+            this.aston.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.aston.UseVisualStyleBackColor = true;
+            this.aston.Click += new System.EventHandler(this.aston_Click);
+            // 
+            // sf
+            // 
+            this.sf.BackgroundImage = global::FormulaAddict.Properties.Resources.ferrari;
+            this.sf.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sf.Location = new System.Drawing.Point(0, 488);
+            this.sf.Name = "sf";
+            this.sf.Size = new System.Drawing.Size(412, 122);
+            this.sf.TabIndex = 6;
+            this.sf.Text = "Ferrari";
+            this.sf.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.sf.UseVisualStyleBackColor = true;
+            this.sf.Click += new System.EventHandler(this.sf_Click);
+            // 
+            // haas
+            // 
+            this.haas.BackgroundImage = global::FormulaAddict.Properties.Resources.haas;
+            this.haas.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.haas.Location = new System.Drawing.Point(0, 600);
+            this.haas.Name = "haas";
+            this.haas.Size = new System.Drawing.Size(412, 122);
+            this.haas.TabIndex = 7;
+            this.haas.Text = "Haas";
+            this.haas.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.haas.UseVisualStyleBackColor = true;
+            this.haas.Click += new System.EventHandler(this.haas_Click);
+            // 
+            // mclaren
+            // 
+            this.mclaren.BackgroundImage = global::FormulaAddict.Properties.Resources.mclaren;
+            this.mclaren.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mclaren.Location = new System.Drawing.Point(418, 0);
+            this.mclaren.Name = "mclaren";
+            this.mclaren.Size = new System.Drawing.Size(412, 122);
+            this.mclaren.TabIndex = 8;
+            this.mclaren.Text = "McLaren";
+            this.mclaren.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.mclaren.UseVisualStyleBackColor = true;
+            this.mclaren.Click += new System.EventHandler(this.mclaren_Click);
+            // 
+            // mercedes
+            // 
+            this.mercedes.BackgroundImage = global::FormulaAddict.Properties.Resources.merc;
+            this.mercedes.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mercedes.Location = new System.Drawing.Point(418, 122);
+            this.mercedes.Name = "mercedes";
+            this.mercedes.Size = new System.Drawing.Size(412, 122);
+            this.mercedes.TabIndex = 9;
+            this.mercedes.Text = "Mercedes";
+            this.mercedes.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.mercedes.UseVisualStyleBackColor = true;
+            this.mercedes.Click += new System.EventHandler(this.mercedes_Click);
+            // 
+            // rb
+            // 
+            this.rb.BackgroundImage = global::FormulaAddict.Properties.Resources.redbull;
+            this.rb.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rb.Location = new System.Drawing.Point(418, 244);
+            this.rb.Name = "rb";
+            this.rb.Size = new System.Drawing.Size(412, 122);
+            this.rb.TabIndex = 10;
+            this.rb.Text = "Red Bull";
+            this.rb.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.rb.UseVisualStyleBackColor = true;
+            this.rb.Click += new System.EventHandler(this.rb_Click);
+            // 
+            // williams
+            // 
+            this.williams.BackgroundImage = global::FormulaAddict.Properties.Resources.williams;
+            this.williams.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.williams.Location = new System.Drawing.Point(418, 366);
+            this.williams.Name = "williams";
+            this.williams.Size = new System.Drawing.Size(412, 122);
+            this.williams.TabIndex = 11;
+            this.williams.Text = "Williams";
+            this.williams.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.williams.UseVisualStyleBackColor = true;
+            this.williams.Click += new System.EventHandler(this.williams_Click);
+            // 
+            // drivers
+            // 
+            this.drivers.BackColor = System.Drawing.Color.Transparent;
+            this.drivers.BackgroundImage = global::FormulaAddict.Properties.Resources.vettel;
+            this.drivers.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.drivers.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.drivers.Location = new System.Drawing.Point(1050, 488);
+            this.drivers.Name = "drivers";
+            this.drivers.Size = new System.Drawing.Size(150, 100);
+            this.drivers.TabIndex = 13;
+            this.drivers.Text = "Drivers";
+            this.drivers.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.drivers.UseVisualStyleBackColor = false;
+            this.drivers.Click += new System.EventHandler(this.drivers_Click);
+            // 
+            // keys
+            // 
+            this.keys.BackColor = System.Drawing.Color.Coral;
+            this.keys.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.keys.Location = new System.Drawing.Point(418, 488);
+            this.keys.Multiline = true;
+            this.keys.Name = "keys";
+            this.keys.ReadOnly = true;
+            this.keys.Size = new System.Drawing.Size(250, 314);
+            this.keys.TabIndex = 14;
+            // 
+            // values
+            // 
+            this.values.BackColor = System.Drawing.Color.Coral;
+            this.values.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.values.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.values.Location = new System.Drawing.Point(674, 488);
+            this.values.Multiline = true;
+            this.values.Name = "values";
+            this.values.ReadOnly = true;
+            this.values.Size = new System.Drawing.Size(372, 314);
+            this.values.TabIndex = 15;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FormulaAddict.Properties.Resources.form3Bg;
+            this.ClientSize = new System.Drawing.Size(1200, 800);
+            this.Controls.Add(this.values);
+            this.Controls.Add(this.keys);
+            this.Controls.Add(this.drivers);
+            this.Controls.Add(this.williams);
+            this.Controls.Add(this.rb);
+            this.Controls.Add(this.mercedes);
+            this.Controls.Add(this.mclaren);
+            this.Controls.Add(this.haas);
+            this.Controls.Add(this.sf);
+            this.Controls.Add(this.aston);
+            this.Controls.Add(this.alpine);
+            this.Controls.Add(this.tauri);
+            this.Controls.Add(this.alfaRomeo);
+            this.Controls.Add(this.exitForm3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitForm3;
+        private System.Windows.Forms.Button alfaRomeo;
+        private System.Windows.Forms.Button tauri;
+        private System.Windows.Forms.Button alpine;
+        private System.Windows.Forms.Button aston;
+        private System.Windows.Forms.Button sf;
+        private System.Windows.Forms.Button haas;
+        private System.Windows.Forms.Button mclaren;
+        private System.Windows.Forms.Button mercedes;
+        private System.Windows.Forms.Button rb;
+        private System.Windows.Forms.Button williams;
+        private System.Windows.Forms.Button drivers;
+        private System.Windows.Forms.TextBox keys;
+        private System.Windows.Forms.TextBox values;
+    }
+}
